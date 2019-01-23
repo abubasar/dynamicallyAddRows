8b1e351b955909d39ee17f7a94291ab1be28dd8e
Rabbi <rabby.neo@outlook.com>
2019-01-23 11:45:21 +0600

    Add project files.

---
+++ Assignment2/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Assignment2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+             Dictionary<string, Employee> employeeList = new Dictionary<string,Employee>();
+            for (int i = 0; i < 5; i++)
+             {
+              employeeList.Add(Convert.ToString(i),new Employee());
+            }
+
+            ViewBag.i = 3;
+            EmployeeViewModel model = new EmployeeViewModel() { Employees = employeeList };
+            return View(model);
+        }
+        public ActionResult Post(EmployeeViewModel model)
+        {
+
+            List<Employee> employees = model.Employees.Select(x => x.Value).ToList();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddNewRow()
+        {
+            Dictionary<string, Employee> employeeList = new Dictionary<string, Employee>();
+
+            employeeList.Add(Guid.NewGuid().ToString(), new Employee());
+
+            EmployeeViewModel model = new EmployeeViewModel() { Employees = employeeList };
+            return PartialView("_AddNewRow",model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public ActionResult Save(Employee employee)
+        {
+            Employee emp = new Employee(employee.Name, employee.Email, employee.Phone, employee.Address, employee.DOB);
+
+           
+
+            return View();
+        }
+
+
+    }
+}
---
+++ Assignment2/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment2.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+
+        }
+        public Employee(string name,string email,string phone,string address,string dob)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.Phone = phone;
+            this.Address = address;
+            this.DOB = dob;
+                
+        }
+        [Required]
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string DOB { get; set; }
+    }
+}
